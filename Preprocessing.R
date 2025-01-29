329e713ac7de29daea1b0ea86a162591f63bd2ad
kryptonitecircuit2002 <sribas.chowdhury888@gmail.com>
2025-01-29 18:51:11 +0530

    Update Preprocessing.R

--- Preprocessing.R
+++ Preprocessing.R
@@ -7,17 +7,17 @@ library(tidyverse)
 library(patchwork)
 
 sampleID = "Control_cerbx_script"
-setwd("D:/Transit/Single_Cell+ATAC/Control/ATAC/Data+Analysis/Analysis.Control/Cerebrocortex")
+setwd(".../Cerebrocortex")
 
 #Read files
-input.10X.Control <- Read10X_h5("D:/Transit/Single_Cell+ATAC/Combined_Analysis/Control_sample/filtered_feature_bc_matrix_control.h5")
-fragpath.Control <- "D:/Transit/Single_Cell+ATAC/Combined_Analysis/Control_sample/atac_fragments_control.tsv.gz"
+input.10X.Control <- Read10X_h5(".../filtered_feature_bc_matrix_control.h5")
+fragpath.Control <- ".../atac_fragments_control.tsv.gz"
 metadata.Control <- read.csv(
-  file = "D:/Transit/Single_Cell+ATAC/Combined_Analysis/Control_sample/per_barcode_metrics_control.csv",
+  file = ".../per_barcode_metrics_control.csv",
   header = TRUE,
   row.names = 1
 )
-"D:\Transit\Single_Cell+ATAC\Control\ATAC\Data+Analysis\Analysis.Control\Danio_rerio.GRCz11.105.filtered.gtf"
+
 ## Create Seurat object
 Control <- CreateSeuratObject(counts = input.10X.Control$`Gene Expression`, 
                            assay = 'RNA',
@@ -28,7 +28,7 @@ Control[["percent.mt"]] <- PercentageFeatureSet(Control, pattern = "^MT-")
 
 #Get annotations 
 #annotaion from GTF
-gref.path = "D:/Transit/Single_Cell+ATAC/Control/ATAC/Data+Analysis/Analysis.Control/Danio_rerio.GRCz11.105.filtered.gtf"
+gref.path = ".../Danio_rerio.GRCz11.105.filtered.gtf"
 gene.coords.zf <- rtracklayer::import(gref.path)
 genome(gene.coords.zf) <- 'GRCz11'
 gene.coords.zf <- gene.coords.zf[! is.na(gene.coords.zf$gene_name),]
@@ -56,9 +56,9 @@ Control <- TSSEnrichment(Control, fast = FALSE)
 Control$pct_reads_in_peaks <- Control$atac_peak_region_fragments / Control$atac_fragments * 100
 
 #Peak calling
-system('bash -c "/home/ayush/.local/bin/macs2 callpeak -t /mnt/d/Transit/Single_Cell+ATAC/Combined_Analysis/Control_sample/atac_fragments_control.tsv.gz -g 1.4e+09 -f BED --nomodel --extsize 200 --shift -100 -n Control_cerbx_script --outdir /mnt/d/Transit/Single_Cell+ATAC/Control/ATAC/Data+Analysis/Analysis.Control/Cerebrocortex" ', 
+system('bash -c "/home/ayush/.local/bin/macs2 callpeak -t /mnt/d/.../atac_fragments_control.tsv.gz -g 1.4e+09 -f BED --nomodel --extsize 200 --shift -100 -n Control_cerbx_script --outdir /mnt/d/Transit/Single_Cell+ATAC/Control/ATAC/Data+Analysis/Analysis.Control/Cerebrocortex" ', 
                 wait = TRUE,  ignore.stderr = FALSE,  ignore.stdout = FALSE)
-peak_path <- "D:/Transit/Single_Cell+ATAC/Control/ATAC/Data+Analysis/Analysis.Control/Cerebrocortex/Control_cerbx_script_peaks.narrowPeak"
+peak_path <- ".../Control_cerbx_script_peaks.narrowPeak"
 peaks <- rtracklayer::import(peak_path, format = "narrowPeak")
 peaks <- keepStandardChromosomes(peaks, pruning.mode = 'coarse')
 peaks <- subsetByOverlaps(x = peaks,
